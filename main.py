d00ee03e4751c79773e94e3f797be81a2cf33949
huhinchang <huhinchang@gmail.com>
2023-09-28 00:12:45 -0400

    defensive programming just in case entry is capitalized

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ async def on_message(message):
         and message.channel.id == WISH_CHANNEL_ID
     ):
         for special in WISH_SPECIAL:
-            if special in message.content.lower():
+            if special.lower() in message.content.lower():
                 await message.add_reaction(WISH_SPECIAL[special])
                 return
 
